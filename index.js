c0347464d61cf9b732310b1db025219636cc0bec
liniers <63049664+liniers@users.noreply.github.com>
2025-12-03 21:21:16 +0100

    añadir archivos

---
+++ index.js
@@ -0,0 +1,30 @@
+ 
+let isotipo = document.getElementById("isotipo");
+let grupo = document.getElementById("grupo");
+let siroco = document.getElementById("siroco");
+
+
+const tl = gsap.timeline();
+
+tl.from(isotipo, {
+  opacity: 0,
+  duration: 1,
+  scale: 0.5,
+  rotation: 90,
+  transformOrigin: "center center",
+  ease: "circ.out",
+});
+
+tl.from(grupo, {
+  opacity: 0,
+  duration: 1,
+  x: -12,
+  ease: "circ.out",
+}, "-=0.4");
+
+tl.from(siroco, {
+  opacity: 0,
+  duration: 1,
+  x: -12,
+  ease: "circ.out",
+}, "-=0.9");
